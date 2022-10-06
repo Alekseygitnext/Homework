733311f2dc03bf4c8bf8d7b8a682b6ea13dd8e76
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-06 20:54:49 +1000

    task5

---
+++ task5_by 08.10.22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать числа от -N до N
+Console.Write("Введите число:");
+double Chislo = double.Parse(Console.ReadLine() ?? "0");
+double b = (Chislo * -1);
+if (Chislo > 0)
+{
+    while (b <= Chislo)
+    {
+        Console.Write(" {0}", b);
+        b++;
+    }
+}
+else
+{
+    while (Chislo <= b)
+    {
+        Console.Write(" {0}", Chislo);
+        Chislo++;
+    }
+}
