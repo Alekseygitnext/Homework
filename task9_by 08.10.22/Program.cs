e8c7c3ef978a0b10e9989d64137c3f1989947f02
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-06 21:50:01 +1000

    task9

---
+++ task9_by 08.10.22/Program.cs
@@ -0,0 +1,7 @@
+﻿// Дано число из отрезка [10,99]. Показать наибольшую цифру числа
+int numberA = new Random().Next(1, 100);//1 2 3 4...99
+Console.WriteLine($"Число {numberA}");
+int firstnumber = (numberA % 10);
+int secondnumber = (numberA / 10);
+Console.WriteLine($"первое число {firstnumber}, второе число {secondnumber}");
+Console.WriteLine("Max: " + Math.Max(firstnumber, secondnumber));
