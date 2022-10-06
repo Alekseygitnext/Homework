78cbb3e23c946797398c082eb7b10f0618e1573b
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-06 21:10:52 +1000

    task6

---
+++ task6_by 08.10.22/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите число:");
+int Chislo = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i <= Chislo; i = i + 1)
+{
+    if (!(i % 2 == 1))
+        Console.Write($" {i}");
+}
