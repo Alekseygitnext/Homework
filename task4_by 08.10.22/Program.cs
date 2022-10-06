0c532ea75d42bf853c6aef3605ee35c393bb9e89
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-06 20:02:02 +1000

    task4

---
+++ task4_by 08.10.22/Program.cs
@@ -0,0 +1,11 @@
+﻿// выяснить является ли число четным
+Console.Write("Введите целое число:");
+int pervoeChislo = int.Parse(Console.ReadLine()?? "0");
+if (pervoeChislo% 2==0)
+{
+    Console.WriteLine ("введено четное число");
+}
+else
+{
+   Console.WriteLine ("введено нечетное число");
+}
