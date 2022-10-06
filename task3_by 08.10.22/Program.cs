1dc240ece1f615221eac502e592031ffd6f1748d
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-06 19:45:15 +1000

    task3

---
+++ task3_by 08.10.22/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("Введите первое число:");
+double pervoeChislo = double.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите второе число:");
+double vtoroeChislo = double.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите третье число:");
+double tretieChislo = double.Parse(Console.ReadLine()?? "0");
+
+Console.WriteLine (Math.Max(pervoeChislo, Math.Max(vtoroeChislo, tretieChislo)));
