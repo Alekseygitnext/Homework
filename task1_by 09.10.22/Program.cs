020032696f12f24ba30a3f9602a89b9d71f7e837
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-07 20:17:21 +1000

    Задача 1 к 09.10.22

---
+++ task1_by 09.10.22/Program.cs
@@ -0,0 +1,32 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите цифру от 1 до 7, обозначающую день недели: ");
+int DenNedeli = int.Parse(Console.ReadLine() ?? "0");
+if (DenNedeli <= 7)
+    switch (DenNedeli)
+    {
+        case 1:
+            Console.WriteLine("Понедельник - рабочий день");
+            break;
+        case 2:
+            Console.WriteLine("Вторник - рабочий день");
+            break;
+        case 3:
+            Console.WriteLine("Среда - рабочий день");
+            break;
+        case 4:
+            Console.WriteLine("Четверг - рабочий день");
+            break;
+        case 5:
+            Console.WriteLine("Пятница - рабочий день");
+            break;
+        case 6:
+            Console.WriteLine("Суббота - выходной");
+            break;
+        case 7:
+            Console.WriteLine("Воскресенье - выходной");
+            break;
+    }
+else
+{
+    Console.WriteLine("Введенa цифра не соответствующая дню недели, от 1 до 7");
+}
