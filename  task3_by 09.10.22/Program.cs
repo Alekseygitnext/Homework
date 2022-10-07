af832e10d39e58a9d284452827915bd84794abfb
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-07 21:04:12 +1000

    Задача 3 к 09.10.22

---
+++  task3_by 09.10.22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать номер четверти, показать
+Console.Write("Введите номер четверти: ");
+int chetvert = int.Parse(Console.ReadLine() ?? "0");
+if (chetvert <= 4)
+    switch (chetvert)
+    {
+        case 1:
+            Console.WriteLine("0<X<infinity, 0<Y<infinity");
+            break;
+        case 2:
+            Console.WriteLine("-infinity<X<0, 0<Y<infinity");
+            break;
+        case 3:
+            Console.WriteLine("-infinity<X<0, -infinity<Y<0");
+            break;
+        case 4:
+            Console.WriteLine("0<X<infinity, -infinity<Y<0");
+            break;
+    }
+else
+{
+    Console.WriteLine("Введен неправильный номер четверти");
+}
