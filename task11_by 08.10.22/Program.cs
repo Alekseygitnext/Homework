381e0d68190c98ceb2f0a67601f178702c606588
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-07 10:10:53 +1000

    task11

---
+++ task11_by 08.10.22/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет,вывести остаток
+Console.Write("Vvedite 1 chislo:");
+double pervoeChislo = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Vvedite 2 chislo:");
+double vtoroeChislo = double.Parse(Console.ReadLine() ?? "0");
+double ostatok = (pervoeChislo % vtoroeChislo);
+if (  pervoeChislo % vtoroeChislo == 0)
+{
+    Console.WriteLine("Chislo cratno");
+}
+else
+{
+    Console.WriteLine($"Ostatok ot delenia {ostatok}");
+}
