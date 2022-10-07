2587db297ee3b8916aeb7801f4c6380a16bbfafc
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-07 10:31:54 +1000

    task12

---
+++ task12_by 08.10.22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число:");
+int Chislo = int.Parse(Console.ReadLine() ?? "0");
+
+if (Chislo / 100 != 0)
+{
+    while (Chislo / 100 >= 10)
+    {
+        Chislo = Chislo / 10;
+        Console.WriteLine(Chislo);
+    }
+    Console.Write("третье число ");
+    Console.WriteLine(Chislo % 10);
+
+}
+else
+{
+    Console.WriteLine("Числа нет");
+}
