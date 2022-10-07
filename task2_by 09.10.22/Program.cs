4f0155864b906b5b8afe2153d7c5305722a4dd41
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-07 16:44:57 +1000

    Задача 2 к 09.10.22

---
+++ task2_by 09.10.22/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверить является ли одно квадратом второго
+Console.Write("Введите первое число:");
+double pervoeChislo = double.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите второе число:");
+double vtoroeChislo = double.Parse(Console.ReadLine()?? "0");
+double kvadrat = Math.Pow(vtoroeChislo, 2);
+if (pervoeChislo == kvadrat)
+{
+    Console.WriteLine ("Первое число квадрат второго");
+}
+else
+{
+    Console.WriteLine ("Первое число не квадрат второго");
+}
